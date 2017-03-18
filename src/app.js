3ff0735ea3ad16c00779fbd7965d3b26ebaa6292
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-03-17 23:51:46 -0400

    basic express app setup

--- src/app.js
+++ src/app.js
@@ -1,2 +1,17 @@
+'use strict';
+const express = require("express");
 const config = require("./config.js");
 
+let app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
+
+app.get("/", function(req, res) {
+    res.send("Welcome to the test page");
+});
+
+
+app.listen(3000, function() {
+    console.log("The frontend server is running on port 3000!");
+});
