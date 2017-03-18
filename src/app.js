ff9eb78c29e6b019ccc9b3f5b38108d7aba2bf74
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-03-18 00:37:56 -0400

    Have Twitter API getting the needed objects

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 'use strict';
 const express = require("express");
+const Twit = require("twit");
+const bodyParser = require("body-parser");
 const config = require("./config.js");
+
+let Twitter = new Twit(config);
+const twitOptions = {
+    "screen_name": "GS_HelloKitty",
+    "count": 5
+};
+const screenName = "GS_HelloKitty";
 
 let app = express();
 
@@ -8,7 +17,32 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
 
 app.get("/", function(req, res) {
-    res.send("Welcome to the test page");
+
+    // Get 5 most recent tweets
+    Twitter.get("statuses/user_timeline", twitOptions, function(error, data, response) {
+        if(!error) {
+            let recentTweets = data;
+        }
+
+        // Get 5 Friends
+        Twitter.get("friends/list", twitOptions, function(error, data, response) {
+            if(!error) {
+                let friends = data.users;
+
+                // TODO: Need to get profile image, real name, and screen name of each friend
+            }
+
+         // Get 5 most recent direct messages
+        Twitter.get("direct_messages", {"count": 5}, function(error, data, response) {
+            let messages = data;
+        }); // End direct message retrieval
+
+        }); // End friends list request
+    }); // End recent tweets request
+
+    res.send("Done");
+
+
 });
 
 
