246e5fe03994e40c564b593eb63b51359937af61
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-03-19 01:04:12 -0400

    finished adding user settings

--- src/app.js
+++ src/app.js
@@ -21,56 +21,60 @@ app.set('views', __dirname + '/templates');
     let recentTweets;
     let friends;
     let messages;
+    let userName;
+    let userFriends;
+    let backgroundImageURL;
+    let userAvatarURL;
+
+    // Get user info
+    Twitter.get("users/lookup", {screen_name: screenName}, function(error, data, response) {
+        if(!error) {
+            userName = data[0].name;
+            userFriends = data[0].friends_count;
+            backgroundImageURL = data[0].profile_background_image_url;
+            userAvatarURL = data[0].profile_image_url;
+        } else {
+            console.log("Error getting user info");
+        }
 
     // Get 5 most recent tweets
     Twitter.get("statuses/user_timeline", twitOptions, function(error, data, response) {
-        if(!error) {
+        if (!error) {
             recentTweets = data;
-
-            // Variables needed per tweet
-                // Message content: text
-                // Sender's profile image: user["profile_image_url"
-                // User's profile image: user["profile_background_image_url_https"]
-            // profileImageURL = recentTweets.user["profile_background_image_url_https"];
-                // # of Retweets: retweet_count
-                // # of likes: user["favorites_count"]
-                // Date tweeted: user["created_at"]
+        } else {
+            console.log("Error getting tweets");
         }
 
         // Get 5 Friends
-        Twitter.get("friends/list", twitOptions, function(error, data, response) {
-            if(!error) {
+        Twitter.get("friends/list", twitOptions, function (error, data, response) {
+            if (!error) {
                 friends = data.users;
-
-                // TODO: Need to get profile image, real name, and screen name of each friend
-                console.log(friends[0].profile_image_url);
-                // Variables for each friend
-                    //profile image: profile_image_url
-                    // real name: name
-                    // screen name: screen_name
+            } else {
+                console.log("Error getting friends");
             }
 
-         // Get 5 most recent direct messages
-        Twitter.get("direct_messages", {"count": 5}, function(error, data, response) {
-            if(!error) {
-                messages = data;
-                // Message variables needed for each message
+            // Get 5 most recent direct messages
+            Twitter.get("direct_messages", {"count": 5}, function (error, data, response) {
+                if (!error) {
+                    messages = data;
+                    console.log(messages);
+                    // Message variables needed for each message
                     // Sender's profile image: sender[profile_image_url]
                     // Message body: text
                     // Date sent: sender["created_at"]
-            }
+                } else {
+                    console.log("error getting messages");
+                }
 
-        }); // End direct message retrieval
+            }); // End direct message retrieval
 
         }); // End friends list request
+    }); // end recent tweets request
 
         app.get("/", function(req, res) {
-            console.log(messages[0].text);
-            // console.log(friends);
-            // console.log(recentTweets);
-            res.render("index", {screenName: screenName, tweets: recentTweets, friends: friends, messages: messages});
+            res.render("index", {userAvatarURL: userAvatarURL, screenName: screenName, userName: userName, userFriends: userFriends, backgroundImageURL: backgroundImageURL, tweets: recentTweets, friends: friends, messages: messages});
         }); // end "/" request
-    }); // End recent tweets request
+    }); // End user profile lookup
 
 
 app.listen(3000, function() {
